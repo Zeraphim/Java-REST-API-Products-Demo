c1ee62a0054dbf268d8e10857b3611604012bef5
zeraphimGCash <john.diamante@gcash.com>
2025-01-22 12:52:30 +0800

    Deleted Async package

--- src/main/java/com/g4/AsyncRestApiScorecards/repository/ScoreCardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.g4.AsyncRestApiScorecards.repository;
-
-import com.g4.AsyncRestApiScorecards.entity.ScoreCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScoreCardRepository extends JpaRepository<ScoreCard, Long> {
-}
