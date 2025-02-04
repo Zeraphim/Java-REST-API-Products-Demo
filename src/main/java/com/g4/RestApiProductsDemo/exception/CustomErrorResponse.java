94024cf768fc021a94ffbecde6ad54c989b12624
zeraphimGCash <john.diamante@gcash.com>
2025-02-04 09:50:27 +0800

    CustomErrorResponse for V3 API - Removed timestamp

--- src/main/java/com/g4/RestApiProductsDemo/exception/CustomErrorResponse.java
+++ src/main/java/com/g4/RestApiProductsDemo/exception/CustomErrorResponse.java
@@ -5,14 +5,14 @@ import java.time.LocalDateTime;
 public class CustomErrorResponse {
     private String status; // "error", "informational", "redirectional", "client error", "server error"
     private String message; // The message (if success) or blank (if failed)
-    private LocalDateTime timestamp; // The current timestamp
+    //private LocalDateTime timestamp; // The current timestamp
     private int statusCode; // The HTTP status code
 
     // Constructor
     public CustomErrorResponse(String status, String message, int statusCode) {
         this.status = status;
         this.message = message;
-        this.timestamp = LocalDateTime.now();
+        //this.timestamp = LocalDateTime.now();
         this.statusCode = statusCode;
     }
 
@@ -33,13 +33,13 @@ public class CustomErrorResponse {
         this.message = message;
     }
 
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
+//    public LocalDateTime getTimestamp() {
+//        return timestamp;
+//    }
 
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
+//    public void setTimestamp(LocalDateTime timestamp) {
+//        this.timestamp = timestamp;
+//    }
 
     public int getStatusCode() {
         return statusCode;
